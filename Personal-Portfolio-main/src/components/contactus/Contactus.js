dad7afe1bad1f8c1228980e56073bbd8f919d529
Kaushal Javangula <116291761+kaushalj2000@users.noreply.github.com>
2025-03-07 23:53:31 -0800

    Update Contactus.js

--- Personal-Portfolio-main/src/components/contactus/Contactus.js
+++ Personal-Portfolio-main/src/components/contactus/Contactus.js
@@ -125,7 +125,7 @@ export default function Contactus() {
                         <FiAtSign />
                       </div>
                       <p style={{ color: "#fbd9ad" }}>
-                        mdtonmoy13.mt@gmail.com
+                        kaushal.javangula@gmail.com
                       </p>
                     </a>
                     <a
@@ -135,10 +135,10 @@ export default function Contactus() {
                       <div className="detailsIcon">
                         <FiPhone />
                       </div>
-                      <p style={{ color: "#fbd9ad" }}>+880 1603-550521</p>
+                      <p style={{ color: "#fbd9ad" }}>+1(628)-529-8305</p>
                     </a>
                     <a
-                      href="https://maps.app.goo.gl/iUHJvPAhJXwJayo68"
+                      href="https://maps.app.goo.gl/UTJCGV2dZW5tv7tn6"
                       className="personal-details"
                     >
                       <div className="personal-details">
@@ -146,24 +146,12 @@ export default function Contactus() {
                           <HiOutlineLocationMarker />
                         </div>
                         <p style={{ color: "#fbd9ad" }}>
-                          BOF R/A, Gazipur Cantonment, BOF-1703, Dhaka,
-                          Bangladesh.
+                          350 Arballo Drive, San Francisco, California, USA, 94132.
                         </p>
                       </div>
                     </a>
                   </div>
-                  <div className="contact-map">
-                    <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3643.833618785371!2d90.41265112695315!3d24.036931700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755db9832166e63%3A0x96050e560e6dc4fa!2sBOF%20Central%20Mosque!5e0!3m2!1sen!2sbd!4v1695023265917!5m2!1sen!2sbd"
-                      frameBorder="0"
-                      allowFullScreen=""
-                      aria-hidden="false"
-                      title="Contact Me"
-                      tabIndex="0"
-                      loading="lazy"
-                      className=""
-                    ></iframe>
-                  </div>
+                  
                 </Col>
               </Row>
             </Col>
