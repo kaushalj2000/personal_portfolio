cc688d334c98a4d5aa29fb1014e02c0382273451
Kaushal Javangula <116291761+kaushalj2000@users.noreply.github.com>
2025-03-07 23:45:32 -0800

    Update EducationJourney.js

--- Personal-Portfolio-main/src/components/aboutme/journey/EducationJourney.js
+++ Personal-Portfolio-main/src/components/aboutme/journey/EducationJourney.js
@@ -18,9 +18,6 @@ function EducationJourney() {
         <Link className="btn btn-primary m-1" to="/experiencejourney">
           Experience Journey
         </Link>
-        <Link className="btn btn-primary m-1" to="/technicaljourney">
-          Technical Journey
-        </Link>
         <Link className="btn btn-primary m-1" to="/projectjourney">
           Project Journey
         </Link>
@@ -35,57 +32,39 @@ function EducationJourney() {
           className="vertical-timeline-element--work"
           contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
           contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
-          date="2004 - 2014"
+          date="2023 - 2025"
           iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
           icon={<SchoolIcon />}
         >
           <h4 className="vertical-timeline-element-title">
-            SECONDARY SCHOOL CERTIFICATE
+           San Francisco State University
           </h4>
           <h4 className="vertical-timeline-element-subtitle mt-2">
-            BOF High School, Gazipur
+          San Francisco, California, USA
           </h4>
           <p>
-            Studied here from Class 1 to 10th as a part of secondary school
-            Certificate and gain GPA 5 in my SSC Examination.
+            Pursuing Master of Science in Business Analytics with a GPA of 3.8 out of 4
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
           contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
-          date="2014 - 2018"
+          date="2017 - 2021"
           iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
           icon={<SchoolIcon />}
         >
           <h4 className="vertical-timeline-element-title">
-            DIPLOMA IN ENGINEERING
+           Gokaraju Rangaraju Institute of Engineering and Technology
           </h4>
           <h4 className="vertical-timeline-element-subtitle mt-2">
-            Dhaka Polytechnic Institute, Dhaka
+            Hyderabad, India
           </h4>
           <p>
-            Studied Diploma in Computer Technology from Dhaka Polytechnic
-            institute and gain CGPA 2.98.
-          </p>
+            Studied Bachelor of Technology - BTech in Mechanical Engineering.
+            </p>
         </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
-          date="2019 - 2023"
-          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
-          icon={<SchoolIcon />}
-        >
-          <h4 className="vertical-timeline-element-title">GRADUATION</h4>
-          <h4 className="vertical-timeline-element-subtitle mt-2">
-            Sonargaon University, Dhaka
-          </h4>
-          <p>
-            Studied Bachelor of Science degree in Computer Science and
-            Engineering field from Sonargaon University and gain CGPA 3.44.
-          </p>
-        </VerticalTimelineElement>
+        
 
         <VerticalTimelineElement
           iconStyle={{ background: "#fbd9ad", color: "rgb(78 22 112)" }}
