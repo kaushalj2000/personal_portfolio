622c825ccaa4d30c008b7b0846531a81ca0758eb
Kaushal Javangula <116291761+kaushalj2000@users.noreply.github.com>
2025-03-08 00:00:40 -0800

    Update ResumePage.js

--- Personal-Portfolio-main/src/pages/resume_page/ResumePage.js
+++ Personal-Portfolio-main/src/pages/resume_page/ResumePage.js
@@ -1,16 +1,15 @@
+import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import Particle from "../../Particle";
-import pdf from "../../assets/MD_MAFUJUL_HASAN.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import Zoom from "react-reveal/Zoom";
-import React, { useState, useEffect } from "react";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import pdf from "../../images/Kaushal_Javangula_Data_Analyst.pdf";
 
-const resumeLink =
-  "https://raw.githubusercontent.com/MD-MAFUJUL-HASAN/Personal-Portfolio/main/src/assets/MD_MAFUJUL_HASAN.pdf";
+// Ensure the workerSrc is correctly set for pdfjs
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function Resume() {
   const [width, setWidth] = useState(1200);
@@ -42,7 +41,7 @@ function Resume() {
                 </Button>
               </div>
               <div className="resume d-flex justify-content-center">
-                <Document file={resumeLink}>
+                <Document file={pdf}>
                   <Page pageNumber={1} scale={width > 786 ? 1.6 : 0.4} />
                 </Document>
               </div>
